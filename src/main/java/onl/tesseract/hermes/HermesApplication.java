b91d61cde2edc4c17f3ff365d6e17c5f2c299436
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2022-01-05 20:44:01 +0100

    init JDA

--- src/main/java/onl/tesseract/hermes/HermesApplication.java
+++ src/main/java/onl/tesseract/hermes/HermesApplication.java
@@ -1,13 +1,26 @@
 package onl.tesseract.hermes;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.security.auth.login.LoginException;
 
 @SpringBootApplication
 public class HermesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HermesApplication.class, args);
-	}
+    public static void main(String[] args)
+    {
+        SpringApplication.run(HermesApplication.class, args);
+    }
 
+    @Bean
+    public JDA initJDA(@Value("${jda.token}") final String botToken) throws LoginException
+    {
+        return JDABuilder.createDefault(botToken)
+                     .build();
+    }
 }
