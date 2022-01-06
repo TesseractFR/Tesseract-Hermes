83349c44ad669930922c8ed86fa71c156e3c3098
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2022-01-06 22:50:11 +0100

    trello: create card and send link

--- src/main/java/onl/tesseract/hermes/command/SuggestionCommand.java
+++ src/main/java/onl/tesseract/hermes/command/SuggestionCommand.java
@@ -1,5 +1,8 @@
 package onl.tesseract.hermes.command;
 
+import com.julienvey.trello.domain.Board;
+import com.julienvey.trello.domain.Card;
+import com.julienvey.trello.domain.TList;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
@@ -11,6 +14,20 @@ import java.util.Objects;
 
 @Component
 public class SuggestionCommand implements DiscordCommand {
+
+    private final Board board;
+    private final TList newSuggestionsList;
+
+    public SuggestionCommand(final Board board)
+    {
+        this.board = board;
+        newSuggestionsList = board.fetchLists()
+                                  .stream()
+                                  .filter(list -> list.getName().equals("Suggestions"))
+                                  .findAny()
+                                  .orElseThrow();
+    }
+
     @Override
     public String name()
     {
@@ -29,9 +46,20 @@ public class SuggestionCommand implements DiscordCommand {
         event.deferReply().queue();
         String title = Objects.requireNonNull(event.getOption("titre")).getAsString();
         String description = Objects.requireNonNull(event.getOption("description")).getAsString();
+
+        Card created = createCard(title, description);
+
         event.getHook()
-             .sendMessage("test")
+             .sendMessage(created.getShortUrl())
              .queue();
+    }
+
+    private Card createCard(final String title, final String description)
+    {
+        Card card = new Card();
+        card.setName(title);
+        card.setDesc(description);
+        return newSuggestionsList.createCard(card);
     }
 
     @Override
